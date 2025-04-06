3a29de512a9a896186df7f4d1d582e19a43fe3af
DenisPanshin <by_den@mail.ru>
2025-04-06 20:47:34 +0500

    1

--- market_prj/settings.py
+++ market_prj/settings.py
@@ -137,13 +137,13 @@ USE_TZ = False
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "static"),
 )
 
 AUTH_USER_MODEL = 'authapp.TravelUser'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
